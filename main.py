341595e606dcca6572330c3e06f1bba752778b56
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-20 22:34:10 +0530

    path added to make sure tht it is necessary

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
+from typing import Annotated
+
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str | None = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+async def read_items(q: str, item_id: Annotated[int,Path(title="get the string")]):
+    results = {"items":item_id}
     if q:
         results.update({"q": q})
     return results
