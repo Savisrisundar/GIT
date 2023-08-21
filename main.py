36bb451d8538e6fe3504a94cee1e7cbc4b2ddf71
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-21 10:58:13 +0530

    change1

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ class USER(BaseModel):
 @app.put("/items/{item_id}")
 
 async def update(item_id:int, item:ITEM, user:USER,importance:Annotated[int,Body()]):
-    results={"item_id":item_id,"item":item,"user":user}
+    results={"item_id":item_id,"item":item,"user":user,"importance": importance}
     return results
