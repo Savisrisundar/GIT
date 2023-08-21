c6110e71e92846b66a48968f386007c095a0b179
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-21 17:15:52 +0530

    done with adding examples

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ class IMAGE(BaseModel):
     name:str
     
 class ITEM(BaseModel):
-    name:str
-    price:float
-    tax:float|None=None
-    description:float|None= Field(default=None,title="description",max_length=50)
+    name:str=Field(examples="SAVITHA")
+    price:float=Field(examples="20.7")
+    tax:float|None=None,Field(exaples="10.0")
+    description:float|None= Field(default=None,title="description",max_length=50,examples="hey here")
     tag:list[str]=[]
     image:list[IMAGE]|None=None
 class USER(BaseModel):
