227ca3305fcc9d1946359abc21e135787ee9db09
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-21 14:48:42 +0530

    nested class and subclass

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from fastapi import FastAPI
 from pydantic import  BaseModel,Field
 
 app = FastAPI()
-
+class IMAGE(BaseModel):
+    url:str
+    name:str
+    
 class ITEM(BaseModel):
     name:str
     price:float
     tax:float|None=None
     description:float|None= Field(default=None,title="description",max_length=50)
-    
+    tag:list[str]=[]
+    image:IMAGE|None=None
 class USER(BaseModel):
     username:str
     fullname:str|None
