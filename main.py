e6edd5f30c76c55e653c421f0a45df66a3695c6a
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-21 09:37:25 +0530

    done by adding multiple parameters

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from pydantic import  BaseModel
 
 app = FastAPI()
 
+class ITEM(BaseModel):
+    name:str
+    price:float
+    tax:float|None=None
+    description:float|None=None
+    
+class USER(BaseModel):
+    username:str
+    fullname:str|None
+    
+@app.put("/items/{item_id}")
 
-@app.get("/items/")
-async def read_items(q: str | None = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
+async def update(item_id:int, item:ITEM, user:USER):
+    results={"item_id":item_id,"item":item,"user":user}
     return results
