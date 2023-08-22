2fbae1e3cf9666de4c42f1b83f89e12b6dc52319
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-22 13:05:17 +0530

    added datetime

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Body
+from datetime import datetime, time, timedelta
+from uuid import UUID
+from typing import Annotated
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: str | None = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+@app.put("/items/{item_id}")
+async def read_items(
+    item_id:UUID,
+    start_date_time:Annotated[datetime|None, Body()]=None,
+    end_time:Annotated[time|None,Body()]=None,
+    q: str | None = None):
+    return {"item_id": item_id,
+        "start_datetime": start_date_time,
+        "end_datetime": end_time}
+
