8fa5e23c8c1b40ec98b2e7e8e7c3b890eec358ab
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-30 14:58:35 +0530

    learnt file uploadfile and forms on same prog

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from typing import Annotated
 
 app=FastAPI()
 @app.post("/files/")
-async def read(files:Annotated[bytes,File()]):
-    return {"file":files}
-@app.post("/upload/")
-async def upload(files:UploadFile):
-    return {"file name":files.filename}
+async def read(files:Annotated[bytes,File()],
+               token:Annotated[str, Form()],
+               filesb:UploadFile):
+    return {"file":files,"token":token,"file content":filesb.content_type}
+
