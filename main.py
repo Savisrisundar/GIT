14a09f889adb64c4a62c5aecb830383e8cf407c5
Savitha Sundaresan <savisrisundar@gmail.com>
2023-08-31 15:46:11 +0530

    comepleted

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from typing import Annotated, Union
-from fastapi import Depends, FastAPI
-app = FastAPI()
-async def common_parameters(
-    q: Union[str, None] = None, skip: int = 0, limit: int = 100
-):
-    return {"q": q, "skip": skip, "limit": limit}
-@app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
-@app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
+from fastapi import FastAPI, Depends, Header, HTTPException
+from typing import Annotated
+
+app= FastAPI()
+
+async def verifytoken(xt: Annotated[str,Header()]):
+    if xt !="fake":
+        raise HTTPException(status_code=404,detail="invalid sry!!")
+    
+async def verifykey(xk:Annotated[str,Header()]):
+    if xk!="fake":
+        raise HTTPException(status_code=401,detail="invalid")
+    return xk
+
+@app.get("/items/",dependencies=[Depends(verifytoken),Depends(verifykey)])
+async def read():
+    return {"foo":"item"}
